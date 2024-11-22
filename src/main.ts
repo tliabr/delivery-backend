0aec95078c9004d4c052d6e8ca57e41492753ae5
Thallia Borba Rosa <thalliaborbarosa@gmail.com>
2024-11-22 15:37:21 +0000

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:4000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
-bootstrap();
+
+bootstrap()
+  .then(() => console.log('Server started'))
+  .catch(() => console.error('Bootstrap failed'));
